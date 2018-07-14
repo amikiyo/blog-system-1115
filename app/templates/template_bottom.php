d483815ff588a45bd808ab7e71239a4ce0234a4f
amikiyo <mamikichi88@gmail.com>
2018-07-14 13:04:44 +0900

    ヘッダーフッター 共通化

---
+++ app/templates/template_bottom.php
@@ -0,0 +1,18 @@
+
+        <!-- begin theme-panel-->
+		<!-- delete -->
+        <!-- end theme-panel -->
+
+		<!-- begin scroll to top btn -->
+		<?php include(TEMPLATE_PATH."/footer.php"); ?>
+		<?php include(TEMPLATE_PATH."/scroll_top.php"); ?>
+		<!-- end scroll to top btn -->
+	</div>
+	<!-- end page container -->
+
+	<!-- ================== BEGIN BASE JS ================== -->
+	<?php include(TEMPLATE_PATH."/scripts.php"); ?>
+	<!-- ================== END BASE JS ================== -->
+
+</body>
+</html>
